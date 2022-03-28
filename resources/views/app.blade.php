74e9fe81014009090335364696272d63d94efe15
ralf <ralf@veebisepad.ee>
2022-03-28 11:42:36 +0300

    Update readme & app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,25 +1,14 @@
 <!doctype html>
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta name="description" content="Kingli lilli">
-        <meta name="standards" content="Created by Veebisepad" charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8"/>
         {{-- <script src="https://js.stripe.com/v3/"></script> --}}
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Hajusrakendus</title>
-        
-        @production
-            @php
-                $manifest = json_decode(file_get_contents(public_path('dist/manifest.json')), true);
-            @endphp
-            <script type="module" src="/dist/{{ $manifest['resources/js/app.js']['file'] }}"></script>
-            <link rel="stylesheet" href="/dist/{{ $manifest['resources/js/app.js']['css'][0] }}">
-        @else
-            <script type="module" src="http://localhost:3000/@@vite/client"></script>
-            <script type="module" src="http://localhost:3000/resources/js/app.js"></script>
-        @endproduction
         @routes
+        @vite
     </head>
     <body>
         @inertia
-
     </body>
+</html>
